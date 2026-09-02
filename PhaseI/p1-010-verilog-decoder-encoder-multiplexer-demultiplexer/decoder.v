0be614af980a0e3a2bf0381cd550a8cae2b52bae
dan_d <109141291+i-dan-d@users.noreply.github.com>
2026-09-02 16:44:50 +0700

    p1-010-verilog-decoder-encoder-multiplexer-demultiplexer

---
+++ PhaseI/p1-010-verilog-decoder-encoder-multiplexer-demultiplexer/decoder.v
@@ -0,0 +1,41 @@
+
+module decoder328(output reg [7:0] out, input [2:0] in,input e);
+ always @(*) begin
+ out[0] = (~in[2] & ~in[1] & ~in[0] & e);
+ out[1] = (~in[2] & ~in[1] & in[0] & e);
+ out[2] = (~in[2] & in[1] & ~in[0] & e);
+ out[3] = (~in[2] & in[1] & in[0] & e);
+ out[4] = (in[2] & ~in[1] & ~in[0] & e);
+ out[5] = (in[2] & ~in[1] & in[0] & e);
+ out[6] = (in[2] & in[1] & ~in[0] & e);
+ out[7] = (in[2] & in[1] & in[0] & e);
+ end
+endmodule
+
+module tb_decoder328;
+ wire [7:0] out;
+ reg [2:0] in;
+ reg e;
+ integer x;
+ decoder328 dcd328(out, in, e);
+
+ initial begin
+  
+  $display("| e i2 i1 i0 | 7 6 5 4 3 2 1 0 |");
+  $display("--------------------------------");
+  #5 e = 0;
+  #10 $display("| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
+  #5 e = 1;
+  
+  for (x=0; (x < 8); x=x+1) 
+  begin
+   in = x;
+   #10 $display("| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
+   
+   end
+ end
+endmodule
+
+
