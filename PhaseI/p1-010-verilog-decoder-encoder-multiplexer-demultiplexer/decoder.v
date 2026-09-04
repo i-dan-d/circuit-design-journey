11ce16c2efd4fb92b044253ff7ff9466e95f6974
dan_d <109141291+i-dan-d@users.noreply.github.com>
2026-09-04 18:57:36 +0700

    Python simylation automation script V1(But at this time I almost do not uuderstand how to use Makefilw in dêply

--- PhaseI/p1-010-verilog-decoder-encoder-multiplexer-demultiplexer/decoder.v
+++ PhaseI/p1-010-verilog-decoder-encoder-multiplexer-demultiplexer/decoder.v
@@ -1,6 +1,7 @@
 
 module decoder328(output reg [7:0] out, input [2:0] in,input e);
  always @(*) begin
+ #1;
  out[0] = (~in[2] & ~in[1] & ~in[0] & e);
  out[1] = (~in[2] & ~in[1] & in[0] & e);
  out[2] = (~in[2] & in[1] & ~in[0] & e);
@@ -20,21 +21,35 @@ module tb_decoder328;
  decoder328 dcd328(out, in, e);
 
  initial begin
-  
-  $display("| e i2 i1 i0 | 7 6 5 4 3 2 1 0 |");
-  $display("--------------------------------");
   #5 e = 0;
-  #10 $display("| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+  for (x=0; (x < 8); x=x+1) 
+  begin
+   #5 in = x;
+   if (out === 8'b0) begin
+   $display("PASSED| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
                                                                out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
-  #5 e = 1;
-  
+   end
+   else  begin
+    $display("FAILED| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
+   end
+  end
+  #5 e = 1;in=3'b0;
   for (x=0; (x < 8); x=x+1) 
   begin
    in = x;
-   #10 $display("| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
-                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
+   #5;
+   if (out === (8'b1 << in)) begin
    
+   $display("PASSED| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
    end
+   else  begin
+    $display("FAILED| %b %b  %b  %b  | %b %b %b %b %b %b %b %b |", e, in[2], in[1], in[0],
+                                                               out[7], out[6], out[5], out[4], out[3], out[2], out[1], out[0]);
+   end
+   
+  end
  end
 endmodule
 
--- PhaseI/p1-011-script-simulation-automation/main.py
+++ PhaseI/p1-011-script-simulation-automation/main.py
@@ -1,3 +1,4 @@
+import argparse
 def check_result(logfile):
 	with open(logfile) as f:
 		lines = f.readlines()
@@ -12,7 +13,7 @@ def check_result(logfile):
 		elif "FAIL" in line:
 			total[smlt][0] += 1
 			failed_lines[smlt].append(line)
-		elif "run -all" in line:
+		elif "run" in line:
 			if smlt <0:
 				smlt +=1
 				total.append([0])
@@ -25,15 +26,19 @@ def check_result(logfile):
 				failed_lines.append([])
 	
 	if failed_lines:
-		print("The errors lines:")
 		for x in range(len(failed_lines)):
 			if failed_lines[x]:
 				amount_chr = int(len(failed_lines[x][0]))-3
-				print(amount_chr*'=',x,"th")
+				print(amount_chr*'=',"errors lines",x,"th")
 				for y in failed_lines[x]:
 					print(y, end='')
 				print(len(failed_lines[x][0])*'-')
 	for x in range(len(total)):
-		print(f"{x+1} PASS: {passed[x]} | FALIED: {len(failed_lines[x])} | TOTAL: {total[x]}")
+		print(f"{x+1} PASS: {passed[x][0]} | FALIED: {len(failed_lines[x])} | TOTAL: {total[x][0]}")
 
-check_result("./RCA4.log")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--log", type=str, required=True, help="Path of File data (file.log simulated) ")
+args = parser.parse_args()
+if __name__=="__main__":
+	check_result(args.log)
